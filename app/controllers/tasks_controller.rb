d22d8030af2fe08e442fe4eb5bf807d3be0e519f
Carlos Alan <cardosocarlosalan@gmail.com>
2022-08-16 16:35:11 -0300

    create my first task crud

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task) if @task.save
+  end
+
+  def checked
+    @task = Task.find(params[:id])
+    @task.completed = @task.completed ? false : true
+    @task.save
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'tasks#index'
+  get 'task/:id', to: 'tasks#show', as: :task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'task/:id', to: 'tasks#update'
+  patch 'task/:id', to: 'tasks#checked', as: :completed
+  delete 'task/:id', to: 'tasks#destroy'
 end
